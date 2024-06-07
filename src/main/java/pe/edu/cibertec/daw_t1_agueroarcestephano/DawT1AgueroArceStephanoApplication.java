3da73918f255a2a2e0f6db77e0190ddca2afbc85
Stephano <nafetsgary@outlook.es>
2024-06-07 18:21:00 -0500

    Iniciando examen

---
+++ src/main/java/pe/edu/cibertec/daw_t1_agueroarcestephano/DawT1AgueroArceStephanoApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.daw_t1_agueroarcestephano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawT1AgueroArceStephanoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DawT1AgueroArceStephanoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/edu/cibertec/daw_t1_agueroarcestephano/DawT1AgueroArceStephanoApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.daw_t1_agueroarcestephano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawT1AgueroArceStephanoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
